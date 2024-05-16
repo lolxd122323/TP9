7eccb0eefc572f27207a56c8db02a002cbf933c5
Rocco Gioia <109048248+lolxd122323@users.noreply.github.com>
2024-05-15 22:46:01 -0300

    Update CAutores.js

--- js/CAutores.js
+++ js/CAutores.js
@@ -28,20 +28,16 @@ function crearTabla(titulos, datos) {
     tabla += "</table>";
     return tabla;
   }
-   //A Listar autores Argentinos
   const autoresArgentinos = autores.filter(autor => autor.nacionalidad === "Argentino");
   const tablaAutoresArgentinos = crearTabla(["Nombre", "Apellido", "Mejor Obra"], autoresArgentinos.map(autor => [autor.nombre, autor.apellido, autor.mejorObra]));
   document.write(tablaAutoresArgentinos);
  
-   //B Listar autores que publicaron entre 1960 y 1980
   const autores1960_1980 = autores.filter(autor => autor.anioPublicacion >= 1960 && autor.anioPublicacion <= 1980);
   const tablaAutores1960_1980 = crearTabla(["Nombre", "Apellido", "Mejor Obra", "Año Publicación"], autores1960_1980.map(autor => [autor.nombre, autor.apellido, autor.mejorObra, autor.anioPublicacion]));
   document.write(tablaAutores1960_1980); 
 
-  //C Calcular promedio de edad de publicación
   const promedioEdad = autores.reduce((acumulador, autor) => acumulador + autor.edadPublicacion, 0) / autores.length;
   document.write(`Edad promedio: ${promedioEdad}`);
 
-  //D Listar todos los autores
   const tablaAutores = crearTabla(["Nombre", "Apellido", "Nacionalidad", "Mejor Obra", "Año Publicación", "Edad Publicación"], autores.map(autor => [autor.nombre, autor.apellido, autor.nacionalidad, autor.mejorObra, autor.anioPublicacion, autor.edadPublicacion]));
   document.write(tablaAutores);
